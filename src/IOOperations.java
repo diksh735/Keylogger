5dbdbfe8123ec070ab3e5c1b82a8edd67270b040
lenovo <dikshagoyal03042000@gmail.com>
2022-01-30 20:54:51 +0530

    Initial Commit

---
+++ src/IOOperations.java
@@ -0,0 +1,8 @@
+public interface IOOperations
+{
+	public void initWriter(String fileName);
+	public void writeString(String content);
+	public void closeWriter();
+	public void writeChar(int c);	
+	public void writeEnter();
+}
